efca62dde54139287617c38afe9d0a9b3c029b47
YesiTamayo <112041102+YesiTamayo@users.noreply.github.com>
2023-05-29 16:48:06 +0200

    corrigiendo identacion scrip.js

--- js-brow-1/scrip.js
+++ js-brow-1/scrip.js
@@ -1,15 +1,15 @@
  const input = document.querySelector('#texto');
-        const resultado = document.querySelector('#resultado');
-        const resultadoMayusculas = document.getElementById('resultadoMayusculas')
-        input.addEventListener('input', function() {
-            const texto = input.value.toUpperCase();
-            resultado.textContent = texto;
+ const resultado = document.querySelector('#resultado');
+ const resultadoMayusculas = document.getElementById('resultadoMayusculas')
+ input.addEventListener('input', function() {
+ const texto = input.value.toUpperCase();
+ resultado.textContent = texto;
 
-        });
+ });
 
-        const boton = document.querySelector('.btn');
-        boton.addEventListener('click',function(){
-        const texto = input.value.toUpperCase();
-        resultadoMayusculas.textContent = texto;
-            
-        });
+ const boton = document.querySelector('.btn');
+ boton.addEventListener('click',function(){
+ const texto = input.value.toUpperCase();
+ resultadoMayusculas.textContent = texto;
+ 
+ });
